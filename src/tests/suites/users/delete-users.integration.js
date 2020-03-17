766a5c4692ac6d74d7ef7e2e02c59023ba186fda
Juan Ignacio Paz <jignaciopaz@ovloop.com>
2020-03-16 22:06:39 -0300

    added integration tests

--- src/tests/suites/users/delete-users.integration.js
+++ src/tests/suites/users/delete-users.integration.js
@@ -1,5 +1,41 @@
 const request = require('supertest');
 const app = require('../../../../app');
-const { version } = require('../../../router');
+const {version} = require('../../../router');
 
-module.exports = () => describe('delete /v1/users/:id', () => {});
+module.exports = () =>
+  describe('delete /v1/users/:id', () => {
+    it('should fail with 404 if user is not found', done => {
+      request(app)
+        .delete(`/${version}/users/10000`)
+        .then(response => {
+          expect(response.statusCode).toBe(404);
+          done();
+        });
+    });
+
+    it('it should succeed with 200 and delete user', done => {
+      const randomNumber = Math.floor(Math.random() * Math.floor(1000));
+      const body = {
+        email: `silviagarcia${randomNumber}@test.com`,
+        password: 'password',
+        firstName: 'Silvia',
+        lastName: 'Garcia',
+        userName: 'silviaGarcia65',
+      };
+      let id;
+
+      request(app)
+        .post(`/${version}/users`)
+        .send(body)
+        .then(response => {
+          id = response.body.insertId;
+          console.log('responsee', response.body)
+          request(app)
+            .delete(`/${version}/users/${id}`)
+            .then(response => {
+              expect(response.statusCode).toBe(200);
+              done();
+            });
+        });
+    });
+  });
--- src/tests/suites/users/get-users.integration.js
+++ src/tests/suites/users/get-users.integration.js
@@ -1,5 +1,24 @@
 const request = require('supertest');
 const app = require('../../../../app');
-const { version } = require('../../../router');
+const {version} = require('../../../router');
 
-module.exports = () => describe('get /v1/users/:id', () => {});
+module.exports = () =>
+  describe('get /v1/users/:id', () => {
+    it('should fail with 404 if user is not found', done => {
+      request(app)
+        .get(`/${version}/users/10000`)
+        .then(response => {
+          expect(response.statusCode).toBe(404);
+          done();
+        });
+    });
+
+    it('it should succeed with 200 and get user', done => {
+      request(app)
+        .get(`/${version}/users/1`)
+        .then(response => {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+    });
+  });
